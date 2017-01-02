5828ef7c0af7d495306bf90186fe63e5da6ae48a
Amy Peniston <amy.peniston@gmail.com>
2017-01-02 19:28:16 -0400

    Add filterTodo by searchText and sort

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -57,15 +57,15 @@ describe('TodoAPI', () => {
   describe('filterTodos', () => {
     var todos = [{
       id: 1,
-      text: 'Example 1',
+      text: 'Example One',
       completed: true
     },{
       id: 2,
-      text: 'Example 2',
+      text: 'Example Two',
       completed: false
     },{
       id: 3,
-      text: 'Example 3',
+      text: 'Example Three',
       completed: true
     }];
 
@@ -80,6 +80,30 @@ describe('TodoAPI', () => {
     it('should return incomplete todos when showCompleted is false', () => {
       var showCompleted = false;
       var searchText = '';
+      var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
+
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should sort by completed status', () => {
+      var showCompleted = true;
+      var searchText = '';
+      var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
+
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should return all todos for empty searchText', () => {
+      var showCompleted = true;
+      var searchText = '';
+      var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
+
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should filter todos by searchText', () => {
+      var showCompleted = true;
+      var searchText = 'three';
       var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
 
       expect(filteredTodos.length).toBe(1);
