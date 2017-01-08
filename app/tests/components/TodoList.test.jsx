32c45d6838a00ac9c8ab20228ab83e8d9f36059d
Amy Peniston <amy.peniston@gmail.com>
2017-01-08 10:22:38 -0400

    Fix failing TodoList test

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -1,11 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var {Provider} = require('react-redux');
 var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
+import {configure} from 'configureStore';
 import ConnectedTodoList, {TodoList} from 'TodoList';
-var Todo = require('Todo');
+import ConnectedTodo, {Todo} from 'Todo';
 
 describe('TodoList', () => {
   it('should exist', () => {
@@ -16,13 +18,27 @@ describe('TodoList', () => {
     var todos = [{
       id: 1,
       text: 'Task 1',
+      completed: false,
+      completedAt: undefined,
+      createdAt: 500
     }, {
       id: 2,
-      text: 'Task 2'
+      text: 'Task 2',
+      completed: false,
+      completedAt: undefined,
+      createdAt: 501
     }];
 
-    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
-    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+    var store = configure({
+      todos: todos
+    });
+    var provider = TestUtils.renderIntoDocument(
+      <Provider store={store}>
+          <ConnectedTodoList/>
+      </Provider>
+    );
+    var todoList = TestUtils.scryRenderedComponentsWithType(provider, ConnectedTodoList)[0];
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, ConnectedTodo);
 
     expect(todosComponents.length).toBe(todos.length);
   });
