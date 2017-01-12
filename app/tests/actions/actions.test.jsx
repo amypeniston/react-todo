7149cd01694bf5c862a0241a4b2bb8574c5a3b32
Amy Peniston <amy.peniston@gmail.com>
2017-01-12 18:28:31 -0400

    Fix failing Add Todo tests and create new test for async action

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -1,5 +1,10 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 var expect = require('expect');
+
 var actions = require('actions');
+
+var createMockStore = configureMockStore([thunk]);
 
 describe('Actions', () => {
   it('should generate searchText action', () => {
@@ -14,9 +19,14 @@ describe('Actions', () => {
   it('should generate add todo action', () => {
     var action = {
       type: 'ADD_TODO',
-      text: 'Example Text'
+      todo: {
+        id: 'abc',
+        text: 'Example',
+        completed: false,
+        createdAt: 12342
+      }
     };
-    var res = actions.addTodo(action.text);
+    var res = actions.addTodo(action.todo);
     expect(res).toEqual(action);
   });
 
@@ -36,6 +46,22 @@ describe('Actions', () => {
     expect(res).toEqual(action);
   });
 
+  it('should create todo and dispatch ADD_TODO', (done) => {
+    const store = createMockStore({});
+    const todoText = 'Example todo';
+    store.dispatch(actions.startAddTodo(todoText)).then(() => {
+      const actions = store.getActions();
+
+      expect(actions[0]).toInclude({
+        type: 'ADD_TODO'
+      });
+      expect(actions[0].todo).toInclude({
+        text: todoText
+      });
+      done();
+    }).catch(done);
+  });
+
   it('should generate toggle showCompleted action', () => {
     var action = {
       type: 'TOGGLE_SHOW_COMPLETED'
@@ -51,5 +77,5 @@ describe('Actions', () => {
     };
     var res = actions.toggleTodo(action.id);
     expect(res).toEqual(action);
-  })
+  });
 });
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -30,12 +30,17 @@ describe('Reducers', () => {
     it('should add new todo', () => {
       var action = {
         type: 'ADD_TODO',
-        text: 'Example todo'
+        todo: {
+          id: 'abc',
+          text: 'Example',
+          completed: false,
+          createdAt: 23249
+        }
       };
       var res = reducers.todosReducer(df([]), df(action));
 
       expect(res.length).toEqual(1);
-      expect(res[0].text).toEqual(action.text);
+      expect(res[0]).toEqual(action.todo);
     });
 
     it('should toggle todo by id', () => {
