0e6a93ce3d46d57528c7dddff06d27214df92843
Amy Peniston <amy.peniston@gmail.com>
2017-01-15 16:50:33 -0400

    Add startaddtodos action to fetch existing todos from Firebase

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -11,49 +11,6 @@ describe('TodoAPI', () => {
     expect(TodoAPI).toExist();
   });
 
-  describe('setTodos', () => {
-    it('should set todos when valid todos array entered', () => {
-      var todos = [{
-        id: 11,
-        text: 'Test',
-        completed: true
-      }];
-
-      TodoAPI.setTodos(todos);
-
-      var actualTodos = JSON.parse(localStorage.getItem('todos'));
-      expect(actualTodos).toEqual(todos);
-    });
-
-    it('should not set todos when invalid todos entered', () => {
-      var badTodos = {text: 'Test'};
-
-      TodoAPI.setTodos(badTodos);
-
-      expect(localStorage.getItem('todos')).toBe(null);
-    });
-  });
-
-  describe('getTodos', () => {
-    it('should return empty array for bad localstorage data', () => {
-      var actualTodos = TodoAPI.getTodos();
-      expect(actualTodos).toEqual([]);
-    });
-
-    it('should return todos if valid array in localstorage', () => {
-      var todos = [{
-        id: 11,
-        text: 'Test',
-        completed: true
-      }];
-
-      localStorage.setItem('todos', JSON.stringify(todos));
-
-      var actualTodos = TodoAPI.getTodos();
-      expect(actualTodos).toEqual(todos);
-    });
-  });
-
   describe('filterTodos', () => {
     var todos = [{
       id: 1,
